f8e5278f588ee4ac641c7c35710da952b9a66673
leelaplap <letandat27101998@gmail.com>
2019-11-06 23:50:03 +0700

    up DatabaseTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(CountryTableSeeder::class);
+         $this->call(CityTableSeeder::class);
     }
 }
