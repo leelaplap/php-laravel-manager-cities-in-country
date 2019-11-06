91e76e7ba96f1088828470abd81c7fee2950a208
leelaplap <letandat27101998@gmail.com>
2019-11-07 02:25:07 +0700

    edit option in view/city/edit

--- resources/views/city/edit.blade.php
+++ resources/views/city/edit.blade.php
@@ -6,26 +6,32 @@
         <div class="form-group">
             <label>City Name</label>
             <label>
-                <input type="text" class="form-control" placeholder="Enter City Name" name="city_name" value="{{$city->city_name}}">
+                <input type="text" class="form-control" placeholder="Enter City Name" name="city_name"
+                       value="{{$city->city_name}}">
             </label>
         </div>
         <div class="form-group">
             <label>City Description</label>
             <label>
-                <input type="text" class="form-control" placeholder="Enter City Description" name="city_desc" value="{{$city->city_desc}}">
+                <input type="text" class="form-control" placeholder="Enter City Description" name="city_desc"
+                       value="{{$city->city_desc}}">
             </label>
         </div>
         <div>
             <label>City Image</label>
             <img src="{{asset('storage/'.$city->city_image)}}" alt="image of{{$city->city_name}}">
-            <input type="file" class="form-control-file" name="city_image"  >
+            <input type="file" class="form-control-file" name="city_image">
         </div>
         <div class="form-group">
             <div class="form-group">
                 <label>Select Country</label>
                 <select class="form-control" name="country_id">
                     @foreach($countries as $country)
-                        <option value="{{$country->id}}">{{$country->country_name}}</option>
+                        <option
+                            @if($city->country_id == $country->id)
+                            selected
+                            @endif
+                            value="{{$country->id}}">{{$country->country_name}}</option>
                     @endforeach
                 </select>
             </div>
